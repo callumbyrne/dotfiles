b0eeeed26a1befb48ae6b0004c5945250d73ab14
callumbyrne <callumjbyrne97@gmail.com>
2024-05-07 23:14:32 +1000

    remove lazynvim and change nvchad statusline

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,16 +1,20 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "nord",
+  theme = "nord",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  statusline = {
+    separator_style = "block",
+  },
+
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 return M
