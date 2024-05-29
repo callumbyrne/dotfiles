8f9b5311484883566d7f0ea1b104410bbf61b306
callumbyrne <callumjbyrne97@gmail.com>
2024-05-29 22:35:15 +1000

    add highlight on yank autocmd

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,10 +1,19 @@
 local autocmd = vim.api.nvim_create_autocmd
 local map = vim.keymap.set
 
+-- show references in telescope
 autocmd("LspAttach", {
   callback = function(args)
     vim.schedule(function()
       map("n", "gr", "<cmd> Telescope lsp_references <CR>", { buffer = args.buf, desc = "LSP references" })
     end)
   end,
+})
+
+-- highlight on yank
+autocmd("TextYankPost", {
+  callback = function()
+    require("vim.highlight").on_yank { higroup = "Visual", timeout = 200 }
+  end,
+  desc = "Highlight when yanking",
 })
