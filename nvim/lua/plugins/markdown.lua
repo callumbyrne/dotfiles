4214766756426282eb2ac90439f1086eef608099
Callum Byrne <callumjbyrne97@gmail.com>
2025-02-18 09:56:22 +1100

    chore: comment out markdown.nvim

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,18 +1,20 @@
-return {
-  {
-    "MeanderingProgrammer/markdown.nvim", -- Markdown syntax highlighting
-    -- enabled = false,
-    ft = { "markdown" },
-    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    opts = {
-      bullets = { "•", "○", "◆", "◇" },
-      checkbox = {
-        -- Character that will replace the [ ] in unchecked checkboxes
-        unchecked = "󰄱 ",
-        -- Character that will replace the [x] in checked checkboxes
-        checked = " ",
-      },
-    },
-  },
-}
+return {}
+
+-- return {
+--   {
+--     "MeanderingProgrammer/markdown.nvim", -- Markdown syntax highlighting
+--     -- enabled = false,
+--     ft = { "markdown" },
+--     name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+--     dependencies = { "nvim-treesitter/nvim-treesitter" },
+--     opts = {
+--       bullets = { "•", "○", "◆", "◇" },
+--       checkbox = {
+--         -- Character that will replace the [ ] in unchecked checkboxes
+--         unchecked = "󰄱 ",
+--         -- Character that will replace the [x] in checked checkboxes
+--         checked = " ",
+--       },
+--     },
+--   },
+-- }
