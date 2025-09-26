c174ac0fbd0b58802f2a7ae68c15b4ef8a2a3372
callumbyrne <callumjbyrne97@gmail.com>
2025-09-26 18:16:39 +1000

    go formatting

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,12 +1,30 @@
 local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
 local map = vim.keymap.set
 
 -- show references in telescope
+augroup("LspConfig", { clear = true })
 autocmd("LspAttach", {
   callback = function(args)
     vim.schedule(function()
       map("n", "gr", "<cmd> Telescope lsp_references <CR>", { buffer = args.buf, desc = "LSP references [Telescope]" })
     end)
+  end,
+})
+
+-- Go settings
+augroup("GoSettings", { clear = true })
+autocmd("FileType", {
+  pattern = "go",
+  group = "GoSettings",
+  callback = function()
+    -- Set indentation to 4 columns for tab characters
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+
+    -- IMPORTANT: Use real tabs, not spaces, for Go
+    vim.opt_local.expandtab = false
   end,
 })
 
